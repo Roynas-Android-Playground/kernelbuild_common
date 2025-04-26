b3e0cc6120fc5996f86c8fea3d1b89398e1258d3
Royna2544 <whiteshell2544@naver.com>
2025-04-26 11:54:00 +0900

    Add tb128fu kernel sample

---
+++ samples/tb128fu.py
@@ -0,0 +1,19 @@
+from kernelbuild import KernelBuild
+from argparse import ArgumentParser
+
+class TB128FUKernelBuild(KernelBuild):
+    def __init__(self):
+        super().__init__('Grass', arch='arm64', kernelType='Image', anykernelDir='AnyKernel3')
+    
+    def buildDefconfigList(self) -> 'list[str]':
+        return ['grass-perf_defconfig']
+    
+    def additionalMakeArgs(self) -> 'list[str]':
+        return KernelBuild.ADDITIONALARGS_LLVM_FULL
+
+def main():
+    b = TB128FUKernelBuild()
+    b.build()
+
+if __name__ == '__main__':
+    main()
